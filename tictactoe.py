ac7eda744991b0eacc69e4fad1c5ba905195035f
eifory <42730886+eifory@users.noreply.github.com>
2019-09-02 17:07:32 +0300

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -5,22 +5,16 @@ def get_board(numbers=[0, 1, 2], rows=3):
 
 def validate_board(board):
     if not (all(len(x)==3 for x in board) and len(board) == 3):
-        print('*')
         return False
 
     d = {0:0, 1:0, 2:0}
     for i in board:
         for k in i:
             if not (k in [0, 1, 2]):
-                print(i)
-                print(k)
-                print('-')
                 return False
             d[k] = d[k] + 1
     if not(d[1] == d[2] or d[1] == (d[2] + 1)):
-        print('+')
         return False
-
     return True
 
 def game_finished(board):
